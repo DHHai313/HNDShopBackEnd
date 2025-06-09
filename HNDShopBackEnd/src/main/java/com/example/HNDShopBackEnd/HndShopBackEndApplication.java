5b56e7c77be6d0e243b3e6447467976bff99e58d
DHHai313 <dinhhonghai3132004@gmail.com>
2025-06-09 21:04:46 +0700

    create project

---
+++ HNDShopBackEnd/src/main/java/com/example/HNDShopBackEnd/HndShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.HNDShopBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HndShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HndShopBackEndApplication.class, args);
+	}
+
+}
---
+++ HNDShopBackEnd/src/test/java/com/example/HNDShopBackEnd/HndShopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HNDShopBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HndShopBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
